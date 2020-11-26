3746f0f54192275ad3ed617d28c121ccce525c4e
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-26 18:03:43 +0430

    Add switch example

---
+++ switch_example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo -n "Enter the name of a car brand: "
+read car
+case $car in
+Tesla)
+echo -n "${car}'s factory in the USA."
+;;
+BMW | Mercedes | Audi | Porsche)
+echo -n "${car}'s factory in Germany."
+;;
+Toyoda | Mazda | Mitsubishi | Subaru)
+echo -n "${car}'s factory in Japan."
+;;
+*)
+echo -n "${car} is an unknown car brand."
+;;
+esac
