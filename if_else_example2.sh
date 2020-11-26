7ff62fe269e6a182834bec3313deebb3cb5a7c4d
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-26 18:02:31 +0430

    Add if else 2d exampple

---
+++ if_else_example2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+admin="devdojo"
+read -p "Enter your username? " username
+# Check if the username provided is the admin
+if [[ "${username}" == "${admin}" ]] ; then
+echo "You are the admin user!"
+else
+echo "You are NOT the admin user!"
+fi
