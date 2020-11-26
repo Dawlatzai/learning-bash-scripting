ebf51771ab34c3ac4d7744646e69308ce22b1c7e
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-26 18:02:05 +0430

    Add multiple line comments

--- string_expersions.sh
+++ string_expersions.sh
@@ -9,10 +9,12 @@
 #True if the length of the string is non-zero.
 [[ -n ${string} ]]
 
-#True if the strings are equal. = should be used with the test
-#command for POSIX conformance. When used with the [[
-#command, this performs pattern matching as described above
-#(Compound Commands).
+: '
+True if the strings are equal. = should be used with the test
+command for POSIX conformance. When used with the [[
+command, this performs pattern matching as described above
+(Compound Commands).
+'
 [[ ${string1} == ${string2} ]]
 
 #True if the strings are not equal.
